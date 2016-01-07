597cecab7e686b6e5b43d2886403d912cc5e3bbe
ivanneira <ien1983@gmail.com>
2016-01-07 05:13:29 -0300

    rollback de couchdb a mongodb por falta de recursos

--- db_class.php
+++ db_class.php
@@ -6,51 +6,6 @@
  * Time: 4:15
  */
 
-$couch_dsn = "http://localhost:5984/";
-$couch_db = "clients";
-
-require_once "couch.php";
-require_once "couchClient.php";
-require_once "couchDocument.php";
 
 
-$client = new couchClient($couch_dsn,$couch_db);
-
-/*
-try {
-    $info = $client->getDatabaseInfos();
-} catch (Exception $e) {
-    echo "Error:".$e->getMessage()." (errcode=".$e->getCode().")\n";
-    exit(1);
-}
-var_dump($info);
-*/
-class db_connection{
-
-    function search($query){
-
-        $client = new couchClient('http://127.0.0.1:5984/','clients');
-
-        try{
-            $response = $client;//continuar
-        }catch (Exception $e){
-            return "error02";
-        }
-
-    }
-}
-
-/*
-$song = new stdClass();
-$song->nombre = "testuser";
-
-try {
-    $response = $client->storeDoc($song);
-} catch (Exception $e) {
-    echo "Error: ".$e->getMessage()." (errcode=".$e->getCode().")\n";
-    exit(1);
-}
-print_r($response);
-
-*/
 ?>
